dd485b6a5fa35975e7224bbcb1d4b99ea25bb126
sir <2362446788@qq.com>
2022-03-27 22:12:57 +0800

    feat: add MyPick

--- type-challenges/easy/4-Pick/template.ts
+++ type-challenges/easy/4-Pick/template.ts
@@ -1,1 +1,24 @@
-type MyPick<T, K> = any;
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+// 按照js的思想
+
+// function MyPick(keys, target) {
+//   // 1、创建一个对象
+//   let object = {};
+//   // 2、遍历target，判断在keys中是否存在，存在赋予object
+//   target.forEach((key) => {
+//     if (keys.includes(key)) {
+//       object[key] = keys[key];
+//     }
+//   });
+//   // 3、返回object
+//   return object;
+// }
+
+// 涉及到的知识点
+// 1、in：mapped -> https://www.typescriptlang.org/docs/handbook/2/mapped-types.html#mapping-modifiers
+// 2、T[P]：indexed -> https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html#handbook-content
+// 3、keyof：lookup -> https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+// 4、extends：Generic Constraints -> https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
