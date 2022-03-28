41e666d1d2aea7f390c46283a361952d783ab8b9
sir <2362446788@qq.com>
2022-03-28 21:37:04 +0800

    feat: add TupleToObject

---
+++ type-challenges/easy/11-TupleToObject/template.ts
@@ -0,0 +1,26 @@
+// type TupleToObject<T extends readonly any[]> = any
+
+// 题目分解：
+// 1、返回一个对象
+// 2、遍历数组
+// 3、做输入检测，对象的索引支持string|number|symbol
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P;
+};
+
+// 1、遍历数组 T[number]
+// 2、typeof 转换类型
+
+let str1 = "1";
+type s1 = typeof str1; // 转换为一个string
+
+const str2 = "2";
+type s2 = typeof str2; // 转换为一个 '2' 不允许被修改
+
+const arr1 = [1, "2"];
+type a1 = typeof arr1; // 转换为 (number|string)[]
+
+const arr2 = [1, "2"] as const;
+type a2 = typeof arr2; // 转换为 readonly [1, '2'] 不允许被修改
+
+const tuple: [number, string] = [1, "2"];
