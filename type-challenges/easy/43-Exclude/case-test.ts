281c8693c2806c0d5d8b4a9090e745d1e4750ca2
sir <2362446788@qq.com>
2022-05-31 22:43:54 +0800

    feat: add Exclude

---
+++ type-challenges/easy/43-Exclude/case-test.ts
@@ -0,0 +1,19 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
+  Expect<
+    Equal<
+      MyExclude<"a" | "b" | "c", "a" | "b">,
+      Exclude<"a" | "b" | "c", "a" | "b">
+    >
+  >,
+  Expect<
+    Equal<
+      MyExclude<string | number | (() => void), Function>,
+      Exclude<string | number | (() => void), Function>
+    >
+  >
+];
+
+type a = Exclude<"a" | "b", "a">;
