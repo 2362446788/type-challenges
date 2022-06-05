26df479da9575c8b6942674f904342a7f542044b
sir <2362446788@qq.com>
2022-06-05 09:09:21 +0800

    feat: add Includes

---
+++ type-challenges/easy/898-Includes/template.ts
@@ -0,0 +1,9 @@
+import type { Equal } from "@type-challenges/utils";
+export type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rust
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rust, U>
+  : false;
