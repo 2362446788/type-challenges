a981f89a47540168246157517e2a0365e0cdcc88
sir <2362446788@qq.com>
2022-06-13 23:23:29 +0800

    feat: add MyOmit

---
+++ type-challenges/middle/3-Omit/template.ts
@@ -0,0 +1,11 @@
+// 1、循环T中的key
+// 2、通过as操作符剔除不是P的key
+// 3、返回对应的值
+// type MyOmit<T, K extends keyof T> = {
+//   [P in keyof T as P extends K ? never : P]: T[P];
+// };
+
+// 通过Exclude剔除选项再进行分配
+type MyOmit<T, K extends keyof T> = {
+  [P in Exclude<keyof T, K>]: T[P];
+};
