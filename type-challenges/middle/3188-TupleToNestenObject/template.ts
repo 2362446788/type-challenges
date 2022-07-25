892cfb78fb1477d71c6590df2185244121bfa12d
sir <2362446788@qq.com>
2022-07-25 20:54:54 +0800

    feat: add TupleToNestedObject

--- type-challenges/middle/3188-TupleToNestenObject/template.ts
+++ type-challenges/middle/3188-TupleToNestenObject/template.ts
@@ -1,1 +1,11 @@
-type TupleToNestedObject<T, U> = any
+type TupleToNestedObject<T extends Array<unknown>, U> = T extends [
+  infer F,
+  ...infer Rest,
+]
+  ? {
+    [key in F as F extends PropertyKey ? F : never]: TupleToNestedObject<
+      Rest,
+      U
+    >
+  }
+  : U
