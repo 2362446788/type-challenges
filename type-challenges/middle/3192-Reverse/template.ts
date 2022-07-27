373fce977fa724c9f3361f89f72be5e43cca6259
sir <2362446788@qq.com>
2022-07-27 20:46:47 +0800

    feat: add Reverse

--- type-challenges/middle/3192-Reverse/template.ts
+++ type-challenges/middle/3192-Reverse/template.ts
@@ -1,1 +1,3 @@
-type Reverse<T extends unknown[], U extends unknown[] = []> = T extends [infer F, ...infer R] ? Reverse<R, [F, ...U]> : U;
+// type Reverse<T extends unknown[], U extends unknown[] = []> = T extends [infer F, ...infer R] ? Reverse<R, [F, ...U]> : U;
+
+type Reverse<T extends any[]> = T extends [infer F, ...infer R] ? [...Reverse<R>, F] : T;
