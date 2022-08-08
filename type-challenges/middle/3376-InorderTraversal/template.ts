368d494177965da079b2ffc3aa4122e6b4d93d0f
sir <2362446788@qq.com>
2022-08-08 23:13:11 +0800

    feat: add

--- type-challenges/middle/3376-InorderTraversal/template.ts
+++ type-challenges/middle/3376-InorderTraversal/template.ts
@@ -1,1 +1,15 @@
-type InorderTraversal<T> = any;
+interface TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+type InorderTraversal<T extends TreeNode | null> =
+  [T] extends [TreeNode]
+  ? (
+    [
+      ...InorderTraversal<T['left']>,
+      T['val'],
+      ...InorderTraversal<T['right']>
+    ]
+  )
+  : []
