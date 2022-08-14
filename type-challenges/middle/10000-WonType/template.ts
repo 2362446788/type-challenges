f5f22ed81a4e9eea052fd2c25931c82ffb7241a8
sir <2362446788@qq.com>
2022-08-14 23:25:35 +0800

    feat: add

---
+++ type-challenges/middle/10000-WonType/template.ts
@@ -0,0 +1,36 @@
+type Template = {
+  params: {
+    name: string;
+    age: number;
+  },
+  payload: {
+    total: {
+      num: number;
+      sec: string;
+    }
+  }
+}
+type T1 = {
+  name: string;
+  age: number;
+}
+
+type LinkString<T extends string, U extends string, I extends string> = `${T}${U}${I}`;
+
+type ExpandTemplate<T, P extends keyof T = keyof T, U extends string = ''> =
+  P extends keyof T
+  ? T[P] extends Record<string, any>
+  ? ExpandTemplate<T[P], keyof T[P], U extends `${infer F}${string}` ? `${U}.${P & string}` : `${U}${P & string}`>
+  : `${U}.${P & string}`
+  : U;
+type U<T, P extends keyof T = keyof T> = P extends keyof T ? `${P & string}` : never;
+/* 
+ * 构造一个type：
+ *  1、接收一个type修饰得映射类型
+ *  2、将映射类型支持的key组成字符串的union类型，支持在进行js赋值的时候提示对应的字符串。
+ *     例如：'params' | 'payload' | 'params.name' | 'params.age' | 'payload.total' | 'payload.total.num' | 'payload.total.sec'
+ */
+type T = ExpandTemplate<T1>;
+type TE = ExpandTemplate<Template>;
+type O = U<T1>;
+const temp: ExpandTemplate<Template> = "payload.total.sec";
