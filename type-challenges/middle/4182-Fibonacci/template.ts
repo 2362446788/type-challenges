901ab260d5ea924d308f61c2795f3c18c5230824
sir <2362446788@qq.com>
2022-08-15 21:26:30 +0800

    feat: add Fibonacci

--- type-challenges/middle/4182-Fibonacci/template.ts
+++ type-challenges/middle/4182-Fibonacci/template.ts
@@ -1,1 +1,8 @@
-type Fibonacci<any> = any;
+type Fibonacci<
+  T extends number,
+  N extends number[] = [1],
+  Res extends number[] = [1],
+  Cur extends number[] = [1]
+  > = N['length'] extends T
+  ? Res['length']
+  : Fibonacci<T, [...N, 1], Cur, [...Res, ...Cur]> 
