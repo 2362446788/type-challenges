ed42fecfb4001c5c47a229668c5167088a260ac8
sir <2362446788@qq.com>
2022-08-20 23:52:04 +0800

    feat: add AllCombitions

--- type-challenges/middle/4260-AllCombitions/template.ts
+++ type-challenges/middle/4260-AllCombitions/template.ts
@@ -1,1 +1,15 @@
-type AllCombinations<S> = any
+type String2Union<S extends String> = S extends `${infer L}${infer R}`
+  ? L | String2Union<R>
+  : S;
+
+type AllCombinations<S extends string> = _AllCombinations<String2Union<S>>;
+
+type Combination<A extends string, B extends string> =
+  | A
+  | B
+  | `${A}${B}`
+  | `${B}${A}`;
+
+type _AllCombinations<A extends string, B extends string = A> = A extends A
+  ? Combination<A, _AllCombinations<Exclude<B, A>>>
+  : never;
