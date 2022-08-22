75adefdf0638e89a23e1d9c247ff4ddc669bcea8
sir <2362446788@qq.com>
2022-08-22 20:48:19 +0800

    feat: add template

--- type-challenges/middle/4425-GreatThan/template.ts
+++ type-challenges/middle/4425-GreatThan/template.ts
@@ -1,1 +1,13 @@
-type GreaterThan<T extends number, U extends number> = any
+// your answers
+type NumberToTuple<N extends number, R extends 1[] = []> = R["length"] extends N
+  ? R
+  : NumberToTuple<N, [1, ...R]>;
+
+type GreaterThan<
+  T extends number,
+  U extends number
+  > = NumberToTuple<T> extends [...NumberToTuple<U>, ...infer R]
+  ? R extends []
+  ? false
+  : true
+  : false;
