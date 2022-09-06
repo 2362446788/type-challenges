44406f148f84dc5380c892d1a1df5c34b45e0f36
sir <2362446788@qq.com>
2022-09-06 23:43:25 +0800

    feat: add Chunk

--- type-challenges/middle/4499-Chunk/template.ts
+++ type-challenges/middle/4499-Chunk/template.ts
@@ -1,1 +1,12 @@
-type Chunk<T> = any
+type Chunk<
+  T,
+  D extends number,
+  A extends any[] = [],
+  TL extends any[] = []
+> = T extends [infer F, ...infer R]
+  ? A["length"] extends D
+  ? Chunk<R, D, [F], [...TL, [...A]]>
+  : Chunk<R, D, [...A, F], TL>
+  : A extends []
+  ? [...TL]
+  : [...TL, A];
