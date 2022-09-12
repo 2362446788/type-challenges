d552920d10c40e0aaffbff31f1a01d2b25f31dd5
sir <2362446788@qq.com>
2022-09-12 21:43:36 +0800

    feat: add Fill

--- type-challenges/middle/4518-Fill/template.ts
+++ type-challenges/middle/4518-Fill/template.ts
@@ -1,1 +1,31 @@
-type Fill<any> = any;
+// 通过 number 直接可以将元组类型转换为联合类型
+type ArrToUnion<T extends Array<any>> = T[number];
+
+// 数字加1
+type AddOne<T extends number, R extends number[] = []> = R["length"] extends T
+  ? [...R, any]["length"]
+  : AddOne<T, [...R, any]>;
+
+// 计算数组Start~End之间的索引index
+type Section<
+  Start extends number = 0,
+  End extends number = 0,
+  R extends number[] = []
+> = End extends 0
+  ? []
+  : Start extends End
+  ? [...R]
+  : Section<AddOne<Start>, End, [...R, Start]>;
+
+// 替换区间为[0, 1)
+type Fill<
+  T extends unknown[],
+  N,
+  Start extends number = 0,
+  End extends number = T["length"],
+  Val extends any[] = []
+> = T extends [infer F, ...infer R]
+  ? Val["length"] extends ArrToUnion<Section<Start, End>>
+  ? Fill<R, N, Start, End, [...Val, N]>
+  : Fill<R, N, Start, End, [...Val, F]>
+  : Val;
