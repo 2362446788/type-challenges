dc8122aaf127c555180738b6fc28fdbc7a9f5a6a
sir <2362446788@qq.com>
2022-09-17 22:56:41 +0800

    feat: add Without

--- type-challenges/middle/5117-Without/template.ts
+++ type-challenges/middle/5117-Without/template.ts
@@ -1,2 +1,9 @@
-type Without<any> = any;
-// template
+type GetUnion<T extends number[] | number> = T extends number[] ? T[number] : T
+
+type Without<
+  T extends number[], 
+  U extends number[] | number, 
+> = 
+  T extends [infer First, ...infer Rest extends number[]] ? 
+    First extends GetUnion<U> ? Without<Rest, U> : [First, ...Without<Rest, U>] 
+  : T
