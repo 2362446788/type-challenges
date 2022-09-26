8b9a4d4f47726b001aa8b98e0b4d5770dc1b1fac
sir <2362446788@qq.com>
2022-09-26 23:00:07 +0800

    feat: add IndexOf

--- type-challenges/middle/5153-Indexof/template.ts
+++ type-challenges/middle/5153-Indexof/template.ts
@@ -1,1 +1,9 @@
-type IndexOf<any> = any;
+type MyEqual<T, U> =
+  (<G>() => G extends T ? 1 : 2) extends
+  (<G>() => G extends U ? 1 : 2) ? true : false
+
+type IndexOf<T extends unknown[], U, Index extends unknown[] = []> = T extends [infer First, ...infer Rest]
+  ? MyEqual<First, U> extends true
+  ? Index['length']
+  : IndexOf<Rest, U, [any, ...Index]>
+  : -1
