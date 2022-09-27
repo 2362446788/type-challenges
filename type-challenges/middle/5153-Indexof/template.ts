322425cdff0d15ba776a6fb36391d95278459bee
sir <2362446788@qq.com>
2022-09-27 22:24:18 +0800

    feat: repeat

--- type-challenges/middle/5153-Indexof/template.ts
+++ type-challenges/middle/5153-Indexof/template.ts
@@ -1,3 +1,13 @@
+// type MyEqual<T, U> =
+//   (<G>() => G extends T ? 1 : 2) extends
+//   (<G>() => G extends U ? 1 : 2) ? true : false
+
+// type IndexOf<T extends unknown[], U, Index extends unknown[] = []> = T extends [infer First, ...infer Rest]
+//   ? MyEqual<First, U> extends true
+//   ? Index['length']
+//   : IndexOf<Rest, U, [any, ...Index]>
+//   : -1
+
 type MyEqual<T, U> =
   (<G>() => G extends T ? 1 : 2) extends
   (<G>() => G extends U ? 1 : 2) ? true : false
