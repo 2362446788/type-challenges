ed2890d51cb4a5f6829365ac0e882cdf613afa57
sir <2362446788@qq.com>
2022-10-13 22:30:19 +0800

    feat: add Unique

--- type-challenges/middle/5360-Unique/template.ts
+++ type-challenges/middle/5360-Unique/template.ts
@@ -1,1 +1,13 @@
-type Unique<any> = any;
+type IsEqual<T, U> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U  ? 1  : 2)
+  ? true
+  : false;
+
+type IsInclude<Arr extends unknown[], T> = Arr extends [infer Head, ...infer Rest]
+  ? IsEqual<T, Head> extends true
+    ? true
+    : IsInclude<Rest, T>
+  : false
+
+type Unique<T extends unknown[], Result extends unknown[] = []> = T extends [infer Head, ...infer Rest]
+  ? Unique<Rest, IsInclude<Result, Head> extends true ? Result : [...Result, Head]>
+  : Result
