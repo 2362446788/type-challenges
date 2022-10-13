7e783bec89d2683888cca1aca9538f0b1ad6ca15
sir <2362446788@qq.com>
2022-10-13 22:31:11 +0800

    feat: remove isEqual

--- type-challenges/middle/5360-Unique/template.ts
+++ type-challenges/middle/5360-Unique/template.ts
@@ -1,11 +1,7 @@
-type IsEqual<T, U> = (<G>() => G extends T ? 1 : 2) extends (<G>() => G extends U  ? 1  : 2)
-  ? true
-  : false;
-
 type IsInclude<Arr extends unknown[], T> = Arr extends [infer Head, ...infer Rest]
   ? IsEqual<T, Head> extends true
-    ? true
-    : IsInclude<Rest, T>
+  ? true
+  : IsInclude<Rest, T>
   : false
 
 type Unique<T extends unknown[], Result extends unknown[] = []> = T extends [infer Head, ...infer Rest]
