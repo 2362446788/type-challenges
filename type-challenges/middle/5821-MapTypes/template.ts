945fc7d84e55650dac5045d0e88e203a665be398
sir <2362446788@qq.com>
2022-10-20 22:54:47 +0800

    feat: add MapType

--- type-challenges/middle/5821-MapTypes/template.ts
+++ type-challenges/middle/5821-MapTypes/template.ts
@@ -1,1 +1,12 @@
-type MapTypes<any> = any;
+type MapTypes<T extends Record<string, any>, R extends { mapFrom: any, mapTo: any }, U extends R = R> = {
+  [K in keyof T]: R extends any
+  ? T[K] extends R['mapFrom']
+  ? R['mapTo']
+  : T[K] extends U['mapFrom']
+  ? never
+  : T[K]
+  : never
+}
+
+type Case = MapTypes<{ name: string; date: Date }, { mapFrom: string, mapTo: number } | { mapFrom: string, mapTo: Date }>
+//  ^?
