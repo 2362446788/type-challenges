b584cf1791c4c5d9d76635202d4c1ed3bef13508
sir <2362446788@qq.com>
2022-11-01 23:04:23 +0800

    feat: add NumberRange

--- type-challenges/middle/8640-NumberRange/template.ts
+++ type-challenges/middle/8640-NumberRange/template.ts
@@ -1,2 +1,5 @@
-type NumberRange<any> = any;
-// ..
+type NumberRange<L, H, union = never, T extends unknown[] = []> = L extends H
+  ? H | union
+  : T["length"] extends L
+  ? NumberRange<[...T, unknown]["length"], H, L | union, [...T, unknown]>
+  : NumberRange<L, H, union, [...T, unknown]>;
