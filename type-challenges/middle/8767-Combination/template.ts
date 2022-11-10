518730769e741d490135b6e675839c85c80c00b6
sir <2362446788@qq.com>
2022-11-10 23:24:57 +0800

    feat: add

--- type-challenges/middle/8767-Combination/template.ts
+++ type-challenges/middle/8767-Combination/template.ts
@@ -1,1 +1,11 @@
-type Combination<any> = any;
+type Filter<T, U> = T extends []
+  ? []
+  : T extends [infer Head, ...infer Tail]
+  ? Head extends U
+  ? Filter<Tail, U>
+  : [Head, ...Filter<Tail, U>]
+  : never
+
+type Combination<T extends string[]> = T extends []
+  ? never
+  : { [K in keyof T]: T[K] | `${T[K]} ${Combination<Filter<T, T[K]>>}` }[number]
