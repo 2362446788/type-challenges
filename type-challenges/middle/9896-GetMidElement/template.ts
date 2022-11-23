b6f57aa018b0e007180289ab4f5d770e33abe4c2
sir <2362446788@qq.com>
2022-11-23 23:07:12 +0800

    feat: add GetMidElement

--- type-challenges/middle/9896-GetMidElement/template.ts
+++ type-challenges/middle/9896-GetMidElement/template.ts
@@ -1,1 +1,14 @@
-type GetMiddleElement<any> = any;
+type Double<T extends unknown[]> = [...T, ...T]
+
+type GetMiddleElement<
+  T extends unknown[],
+  I extends unknown[] = []
+> = I['length'] extends T['length']
+  ? []
+  : [...I, unknown]['length'] extends number
+  ? [...Double<I>, unknown]['length'] extends T['length']
+  ? [T[I['length']]]
+  : Double<[...I, unknown]>['length'] extends T['length']
+  ? [T[I['length']], T[[...I, unknown]['length']]]
+  : GetMiddleElement<T, [...I, unknown]>
+  : never
