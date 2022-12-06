7e65b62d51f1b12f43d765645736d9fa793a07c4
sir <2362446788@qq.com>
2022-12-06 21:54:00 +0800

    feat: add ToPrimitive

--- type-challenges/middle/16259-toPrimitive/template.ts
+++ type-challenges/middle/16259-toPrimitive/template.ts
@@ -1,1 +1,10 @@
-type ToPrimitive<any> = any;
+// your answers
+type ToPrimitive<T> = {
+  [K in keyof T]: T[K] extends string
+  ? string
+  : T[K] extends number
+  ? number
+  : T[K] extends boolean
+  ? boolean
+  : ToPrimitive<T[K]>
+}
