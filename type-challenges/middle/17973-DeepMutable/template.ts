f9ed393cec60fe574863de1d4397ce10e5d0849f
sir <2362446788@qq.com>
2022-12-25 23:01:53 +0800

    feat: add

--- type-challenges/middle/17973-DeepMutable/template.ts
+++ type-challenges/middle/17973-DeepMutable/template.ts
@@ -1,1 +1,9 @@
-type DeepMutation<any> = any;
+type DeepMutation<any> = any; type DeepMutable<T> = {
+  -readonly [key in keyof T]: T[key] extends any[]
+  ? DeepMutable<T[key]>
+  : T[key] extends Function
+  ? T[key]
+  : T[key] extends object
+  ? DeepMutable<T[key]>
+  : T[key];
+};
