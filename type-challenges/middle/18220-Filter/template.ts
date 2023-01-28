bd5dba9cdbfa623400690539700cd6611a5840fa
sir <2362446788@qq.com>
2023-01-28 22:31:46 +0800

    feat: add

--- type-challenges/middle/18220-Filter/template.ts
+++ type-challenges/middle/18220-Filter/template.ts
@@ -1,1 +1,3 @@
-type Filter = any;
+type Filter<T extends any[], P, R extends any[] = []> = T extends [infer First, ...infer Rest] ? (
+  Filter<Rest, P, First extends P ? [...R, First] : R>
+) : R
--- type-challenges/middle/4260-AllCombitions/template.ts
+++ type-challenges/middle/4260-AllCombitions/template.ts
@@ -4,14 +4,14 @@ type String2Union<S extends String> = S extends `${infer L}${infer R}`
 
 type AllCombinations<S extends string> = _AllCombinations<String2Union<S>>;
 
-type Combination<A extends string, B extends string> =
+type CombinationA<A extends string, B extends string> =
   | A
   | B
   | `${A}${B}`
   | `${B}${A}`;
 
 type _AllCombinations<A extends string, B extends string = A> = A extends A
-  ? Combination<A, _AllCombinations<Exclude<B, A>>>
+  ? CombinationA<A, _AllCombinations<Exclude<B, A>>>
   : never;
 
 type TT = String2Union<'a'>
