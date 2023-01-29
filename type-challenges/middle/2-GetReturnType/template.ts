18b4c7147364a5b71f2d891f806b2965e74da3b0
sir <2362446788@qq.com>
2023-01-29 22:52:13 +0800

    feat: add GetReturnType

--- type-challenges/middle/2-GetReturnType/template.ts
+++ type-challenges/middle/2-GetReturnType/template.ts
@@ -1,5 +1,7 @@
-type MyReturnType<T extends Function> = T extends (
-  ...args: any[]
-) => infer ReturnType
-  ? ReturnType
-  : never;
+// type MyReturnType<T extends Function> = T extends (
+//   ...args: any[]
+// ) => infer ReturnType
+//   ? ReturnType
+//   : never;
+
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any[]) => infer R ? R : never;
