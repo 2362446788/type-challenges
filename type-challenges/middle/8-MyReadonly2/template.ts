5ea34ced4e01f91be6397421a0a11d4c96bf3633
sir <2362446788@qq.com>
2023-01-31 23:27:03 +0800

    feat: MyReadonly2

--- type-challenges/middle/8-MyReadonly2/template.ts
+++ type-challenges/middle/8-MyReadonly2/template.ts
@@ -5,6 +5,8 @@
 // type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> &
 //   Readonly<Pick<T, K>>;
 
-type MyReadonly2<T, K extends keyof T = keyof T> = {
-  [P in keyof T as P extends K ? never : P]: T[P];
-} & { readonly [P in K]: T[P] };
+// type MyReadonly2<T, K extends keyof T = keyof T> = {
+//   [P in keyof T as P extends K ? never : P]: T[P];
+// } & { readonly [P in K]: T[P] };
+
+type MyReadonly2<T, K extends keyof T = keyof T> = Omit<T, K> & Readonly<Pick<T, K>>;
