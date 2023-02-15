b7576986d11bc538deb3ec53a59293bd9239820b
sir <2362446788@qq.com>
2023-02-15 23:07:54 +0800

    feat: add

--- type-challenges/easy/898-Includes/template.ts
+++ type-challenges/easy/898-Includes/template.ts
@@ -1,9 +1,19 @@
+// import type { Equal } from "@type-challenges/utils";
+// export type Includes<T extends readonly any[], U> = T extends [
+//   infer First,
+//   ...infer Rust
+// ]
+//   ? Equal<First, U> extends true
+//     ? true
+//     : Includes<Rust, U>
+//   : false;
+
 import type { Equal } from "@type-challenges/utils";
 export type Includes<T extends readonly any[], U> = T extends [
   infer First,
   ...infer Rust
 ]
   ? Equal<First, U> extends true
-    ? true
-    : Includes<Rust, U>
+  ? true
+  : Includes<Rust, U>
   : false;
