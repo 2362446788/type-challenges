60f588d6da4e9904adf7b49bd1b109a938d223ae
sir <2362446788@qq.com>
2023-02-18 23:50:48 +0800

    feat: add

--- type-challenges/easy/3060-Unshift/template.ts
+++ type-challenges/easy/3060-Unshift/template.ts
@@ -1,3 +1,6 @@
+// type Unshift<T extends unknown[], U> = T extends [...infer Rust]
+//   ? [U, ...Rust]
+//   : never;
 type Unshift<T extends unknown[], U> = T extends [...infer Rust]
   ? [U, ...Rust]
   : never;
