3a4265ebe396723186b86afdcaa1ac4c593522aa
sir <2362446788@qq.com>
2023-02-27 23:56:01 +0800

    feat: add

--- type-challenges/middle/12-ChainAbleOptions/template.ts
+++ type-challenges/middle/12-ChainAbleOptions/template.ts
@@ -2,13 +2,13 @@
 // 2、限制options的参数，如果是之前做过的key就需要舍弃
 // 3、需要进行递归处理添加的key
 // 4、最后通过get返回构造的数据
-type Chainable<R = {}> = {
-  option<K extends string, V = any>(
-    key: K extends keyof R ? never : K,
-    value: V
-  ): Chainable<R & { [key in K]: V }>;
-  get(): R;
-};
+// type Chainable<R = {}> = {
+//   option<K extends string, V = any>(
+//     key: K extends keyof R ? never : K,
+//     value: V
+//   ): Chainable<R & { [key in K]: V }>;
+//   get(): R;
+// };
 
 // type Chainable<Prev = {}> = {
 //   option<K extends string, V = any>(
@@ -17,3 +17,10 @@ type Chainable<R = {}> = {
 //   ): Chainable<Prev & { [key in K]: V }>;
 //   get(): Prev;
 // };
+type Chainable<R = {}> = {
+  option<K extends string, V = any>(
+    key: K extends keyof R ? never : K,
+    value: V
+  ): Chainable<R & { [key in K]: V }>;
+  get(): R;
+};
