f7acc0af03a8fc28c4e9e5997086d30c9c5cac07
sir <2362446788@qq.com>
2023-03-02 23:27:33 +0800

    feat: add

--- type-challenges/middle/16-Pop/template.ts
+++ type-challenges/middle/16-Pop/template.ts
@@ -1,3 +1,4 @@
 // 1、通过infer推断出前面的数据和最后一位数据
 // 2、将前面的数据输出
+// type Pop<T extends unknown[]> = T extends [...infer R, unknown] ? R : never;
 type Pop<T extends unknown[]> = T extends [...infer R, unknown] ? R : never;
