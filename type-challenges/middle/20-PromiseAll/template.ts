508c6129a7bd4f95b771cd418f96a5ca2697194d
sir <2362446788@qq.com>
2023-03-03 23:43:53 +0800

    feat: add

--- type-challenges/middle/20-PromiseAll/template.ts
+++ type-challenges/middle/20-PromiseAll/template.ts
@@ -1,3 +1,8 @@
+// declare function PromiseAll<T extends any[]>(
+//   values: readonly [...T]
+// ): Promise<{
+//   [K in keyof T]: T[K] extends Promise<infer P> ? P : T[K];
+// }>;
 declare function PromiseAll<T extends any[]>(
   values: readonly [...T]
 ): Promise<{
