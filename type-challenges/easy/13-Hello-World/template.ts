a0286910e64456b9eab65a406bc0297aae542b2e
sir <2362446788@qq.com>
2023-08-29 23:15:38 +0800

    feat: HelloWorld

--- type-challenges/easy/13-Hello-World/template.ts
+++ type-challenges/easy/13-Hello-World/template.ts
@@ -1,2 +1,3 @@
+type HelloWorld = string;
 // type HelloWorld = string; // expected to be a string
-type HelloWorld = string; // expected to be a string
+// type HelloWorld = string; // expected to be a string
