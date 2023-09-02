182fdddf32f8f5258d6febadbc1389fcc2150552
sir <2362446788@qq.com>
2023-09-02 22:54:07 +0800

    feat: First

--- type-challenges/easy/14-First/template.ts
+++ type-challenges/easy/14-First/template.ts
@@ -6,7 +6,8 @@
 //   : never;
 
 // type First<T extends any[]> = T extends [] ? never : T[0];
-type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+// type First<T extends any[]> = T["length"] extends 0 ? never : T[0];
+type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
 
 // 1、extends 方式
 // 2、tuple 的 length
