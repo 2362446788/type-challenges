f0b43b2e44395481239258c4f3679cc43ececa6d
sir <2362446788@qq.com>
2023-09-07 22:36:04 +0800

    feat: MyAwaited

--- type-challenges/easy/189-Awaited/template.ts
+++ type-challenges/easy/189-Awaited/template.ts
@@ -3,9 +3,13 @@
 //     ? MyAwaited<X>
 //     : X
 //   : T;
-type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
-  ? X extends Promise<unknown>
-  ? MyAwaited<X>
-  : X
-  : T;
-
+// type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
+//   ? X extends Promise<unknown>
+//   ? MyAwaited<X>
+//   : X
+//   : T;
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer K>
+  ? K extends Promise<unknown>
+    ? MyAwaited<K>
+    : K
+  : never;
