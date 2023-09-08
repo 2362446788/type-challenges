20b889fd17eaf72284ecbc2de23580c7d87eca18
sir <2362446788@qq.com>
2023-09-08 22:43:03 +0800

    feat: If

--- type-challenges/easy/268-If/template.ts
+++ type-challenges/easy/268-If/template.ts
@@ -1,7 +1,7 @@
 // type If<C extends boolean, T extends any, F extends any> = C extends true
 //   ? T
 //   : F;
-type If<C extends boolean, T extends any, F extends any> = C extends true
-  ? T
-  : F;
-
+// type If<C extends boolean, T extends any, F extends any> = C extends true
+//   ? T
+//   : F;
+type If<T extends boolean, K, U> = T extends true ? K : U;
