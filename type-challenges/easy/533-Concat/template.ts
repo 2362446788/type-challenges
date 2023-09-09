22360e9c44a9101b1683483b045f2636032df5df
sir <2362446788@qq.com>
2023-09-09 22:58:26 +0800

    feat: Concat

--- type-challenges/easy/533-Concat/template.ts
+++ type-challenges/easy/533-Concat/template.ts
@@ -5,10 +5,11 @@
 //     ? [...First, ...Second]
 //     : [...First]
 //   : [];
-type Concat<T extends unknown[], U extends unknown[]> = T extends [
-  ...infer First
-]
-  ? U extends [...infer Second]
-  ? [...First, ...Second]
-  : [...First]
-  : [];
+// type Concat<T extends unknown[], U extends unknown[]> = T extends [
+//   ...infer First
+// ]
+//   ? U extends [...infer Second]
+//   ? [...First, ...Second]
+//   : [...First]
+//   : [];
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
