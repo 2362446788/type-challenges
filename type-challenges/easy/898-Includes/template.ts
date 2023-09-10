b5a4388b586e773e3bd6c684ce47e8e45d6ae91f
sir <2362446788@qq.com>
2023-09-10 22:45:52 +0800

    feat: Includes

--- type-challenges/easy/898-Includes/template.ts
+++ type-challenges/easy/898-Includes/template.ts
@@ -9,11 +9,17 @@
 //   : false;
 
 import type { Equal } from "@type-challenges/utils";
-export type Includes<T extends readonly any[], U> = T extends [
-  infer First,
-  ...infer Rust
-]
-  ? Equal<First, U> extends true
-  ? true
-  : Includes<Rust, U>
+// export type Includes<T extends readonly any[], U> = T extends [
+//   infer First,
+//   ...infer Rust
+// ]
+//   ? Equal<First, U> extends true
+//   ? true
+//   : Includes<Rust, U>
+//   : false;
+
+export type Includes<T extends unknown[], U> = T extends [infer F, ...infer R]
+  ? Equal<F, U> extends true
+    ? true
+    : Includes<R, U>
   : false;
