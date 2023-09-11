b7a530386ad41c1136660476d6a28cc50813769a
sir <2362446788@qq.com>
2023-09-11 22:18:48 +0800

    feat: Push

--- type-challenges/easy/3057-Push/template.ts
+++ type-challenges/easy/3057-Push/template.ts
@@ -1,6 +1,7 @@
 // type Push<T extends unknown[], U> = T extends [...infer Rust]
 //   ? [...Rust, U]
 //   : never;
-type Push<T extends unknown[], U> = T extends [...infer Rust]
-  ? [...Rust, U]
-  : never;
+// type Push<T extends unknown[], U> = T extends [...infer Rust]
+//   ? [...Rust, U]
+//   : never;
+type Push<T extends unknown[], U> = [...T, U];
