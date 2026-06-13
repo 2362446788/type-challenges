129c92b590e782e5e93edf7c9016d1a99ed90382
wang_tao <2362446788@qq.com>
2026-06-13 21:04:02 +0800

    feat: 2026-06-13 2070

--- type-challenges/middle/2070-DropChar/case-test.ts
+++ type-challenges/middle/2070-DropChar/case-test.ts
@@ -1,12 +1,24 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
+
+// 使用模版字符串匹配 F 和 R
+// 然后返回新结果，判断 F 是否和 V 是相同字符
+// 是：将其变成返回空字符
+// 否：继续返回 F
+// 后续的字符串进行递归调用
+type DropChar<
+  T extends string,
+  V extends string,
+> = T extends `${infer F}${infer R}`
+  ? `${F extends V ? "" : F}${DropChar<R, V>}`
+  : T;
 
 type cases = [
   // @ts-expect-error
-  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
-  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
-  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
-  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
-  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
-  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
-  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
-]
+  Expect<Equal<DropChar<"butter fly!", "">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<"butter fly!", "!">, "butter fly">>,
+  Expect<Equal<DropChar<"    butter fly!        ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", " ">, "butterfly!">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
+  Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>,
+];
