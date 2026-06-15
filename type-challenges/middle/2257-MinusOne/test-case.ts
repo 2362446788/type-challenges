d4ca3c809acd2f3cf2e84c2269c5a85ebdda5da3
wang_tao <2362446788@qq.com>
2026-06-15 09:39:55 +0800

    feat: 2026-06-15

--- type-challenges/middle/2257-MinusOne/test-case.ts
+++ type-challenges/middle/2257-MinusOne/test-case.ts
@@ -1,4 +1,17 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
+
+// 构建数组
+type CreateArray<T extends number, U extends any[] = []> = U["length"] extends T
+  ? U
+  : CreateArray<T, [...U, any]>;
+
+// 借助数组来实现删除
+type MinusOne<T extends number, U = CreateArray<T>> = U extends [
+  infer _,
+  ...infer R,
+]
+  ? R["length"]
+  : never;
 
 type cases = [
   Expect<Equal<MinusOne<1>, 0>>,
@@ -6,4 +19,4 @@ type cases = [
   Expect<Equal<MinusOne<3>, 2>>,
   Expect<Equal<MinusOne<100>, 99>>,
   Expect<Equal<MinusOne<1101>, 1100>>,
-]
+];
