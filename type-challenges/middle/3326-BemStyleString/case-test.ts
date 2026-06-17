874d60fff88b49ade8f15b67410465a41bbc62d1
wang_tao <2362446788@qq.com>
2026-06-17 10:19:23 +0800

    feat: 3326

--- type-challenges/middle/3326-BemStyleString/case-test.ts
+++ type-challenges/middle/3326-BemStyleString/case-test.ts
@@ -1,7 +1,33 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
+
+// 使用 union 类型在字符串中也会触发分布式计算的能力
+// 例如：T = 'a' V = 'b' | 'c'
+// U = `${T}${V}`
+type T = "a";
+type V = "b" | "c";
+type U = `${T}${V}`; // 'ab' | 'ac'
+type BEM<
+  T extends string,
+  V extends string[],
+  U extends string[],
+> = `${T}${V["length"] extends 0 ? "" : `__${V[number]}`}${U["length"] extends 0 ? "" : `--${U[number]}`}`;
+
+type T1 = BEM<"btn", ["price"], []>;
+type T2 = BEM<"btn", ["price"], ["warning", "success"]>;
+type T3 = BEM<"btn", [], ["small", "medium", "large"]>;
 
 type cases = [
-  Expect<Equal<BEM<'btn', ['price'], []>, 'btn__price'>>,
-  Expect<Equal<BEM<'btn', ['price'], ['warning', 'success']>, 'btn__price--warning' | 'btn__price--success'>>,
-  Expect<Equal<BEM<'btn', [], ['small', 'medium', 'large']>, 'btn--small' | 'btn--medium' | 'btn--large'>>,
-]
+  Expect<Equal<BEM<"btn", ["price"], []>, "btn__price">>,
+  Expect<
+    Equal<
+      BEM<"btn", ["price"], ["warning", "success"]>,
+      "btn__price--warning" | "btn__price--success"
+    >
+  >,
+  Expect<
+    Equal<
+      BEM<"btn", [], ["small", "medium", "large"]>,
+      "btn--small" | "btn--medium" | "btn--large"
+    >
+  >,
+];
