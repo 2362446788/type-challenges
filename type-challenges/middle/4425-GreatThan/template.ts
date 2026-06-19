e9ab748b1c71e9d15817a88e3be6d34cd891e9ff
wang_tao <2362446788@qq.com>
2026-06-19 21:07:59 +0800

    feat: 4425

--- type-challenges/middle/4425-GreatThan/template.ts
+++ type-challenges/middle/4425-GreatThan/template.ts
@@ -3,11 +3,9 @@ type NumberToTuple<N extends number, R extends 1[] = []> = R["length"] extends N
   ? R
   : NumberToTuple<N, [1, ...R]>;
 
-type GreaterThan<
-  T extends number,
-  U extends number
-  > = NumberToTuple<T> extends [...NumberToTuple<U>, ...infer R]
-  ? R extends []
-  ? false
-  : true
-  : false;
+type GreaterThan<T extends number, U extends number> =
+  NumberToTuple<T> extends [...NumberToTuple<U>, ...infer R]
+    ? R extends []
+      ? false
+      : true
+    : false;
--- type-challenges/middle/4425-GreatThan/test-case.ts
+++ type-challenges/middle/4425-GreatThan/test-case.ts
@@ -1,4 +1,15 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type CreateArray<N extends number, V extends any[] = []> = V["length"] extends N
+  ? V
+  : CreateArray<N, [...V, any]>;
+
+// ts 中无法直接进行数学运算，只能通过数组来进行操作
+// 因此写一个根据数字来创建数组的类型 CreateArray
+// T > U，即 T 里面包含了所有的 U，并且至少多了一个值
+// 可以使用 extends 进行比较 CreateArray<T> extends [...CreateArray<U>, any, ...infer _]
+type GreaterThan<T extends number, U extends number> =
+  CreateArray<T> extends [...CreateArray<U>, any, ...infer _] ? true : false;
 
 type cases = [
   Expect<Equal<GreaterThan<1, 0>, true>>,
@@ -8,4 +19,4 @@ type cases = [
   Expect<Equal<GreaterThan<20, 20>, false>>,
   Expect<Equal<GreaterThan<10, 100>, false>>,
   Expect<Equal<GreaterThan<111, 11>, true>>,
-]
+];
