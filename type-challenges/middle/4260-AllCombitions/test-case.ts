b97483d446942fdbc2fec5d08acdb48054716590
wang_tao <2362446788@qq.com>
2026-06-19 20:55:36 +0800

    feat: 4260

--- type-challenges/middle/4260-AllCombitions/test-case.ts
+++ type-challenges/middle/4260-AllCombitions/test-case.ts
@@ -1,11 +1,27 @@
 import type { Equal, Expect } from "@type-challenges/utils";
 
+// type StringToUnion<S extends string> = S extends `${infer F}${infer R}`
+//   ? F | StringToUnion<R>
+//   : S;
+
 type StringToUnion<S extends string> = S extends `${infer F}${infer R}`
   ? F | StringToUnion<R>
   : S;
 type T1 = StringToUnion<"">; // ""
 type T2 = StringToUnion<"A">; // "" | "A"
 type T3 = StringToUnion<"AB">; // "" | "A" | "B"
+
+// 最新版：只是用一个 AllCombinations 处理
+// 将需要进行 union 的数据使用泛型保存到 T 和 U
+// 每次遍历将获取到的第一个字符和剩余字符递归执行的结果进行联合
+// 但是需要注意⚠️：递归调用需要排除当前 T 的值
+type AllCombinations<
+  S extends string,
+  T extends string = StringToUnion<S>,
+  U extends string = T,
+> = T extends any ? T | `${T}${AllCombinations<S, Exclude<U, T>>}` : never;
+
+type T4 = AllCombinations<"ABC">;
 
 // 题目输入是字符串，输出是 union，字符串是没办法做分布式条件的，所以需要将 string 转为 union
 // 1. 实现 Combination 来处理，接受字符串 union 类型，F 用来触发分布式计算，U 用来保存 F 的值，触发分布式计算之后，返回 F ｜ `${F}${Exclude<U, F>}`
@@ -14,11 +30,11 @@ type T3 = StringToUnion<"AB">; // "" | "A" | "B"
 //   : "";
 // type C = Combination<"" | "A" | "B" | "C">; // "" | "A" | "AB" | "B" | "C" | "AC" | "BA" | "BC" | "CA" | "CB"
 // 2. F ｜ `${F}${Exclude<U, F>}` 这种方式只有剩余项和 F 的联合，没有触发剩余项自己的联合，不满足，所以需要递归来调用
-type Combination<F extends string, U extends string = F> = F extends any
-  ? F | `${F}${Combination<Exclude<U, F>>}`
-  : "";
-type C = Combination<"" | "A" | "B" | "C">; // "" | "A" | "AB" | "B" | "C" | "BC" | "CB" | "AC" | "ABC" | "ACB" | "CA" | "BA" | "BAC" | "BCA" | "CAB" | "CBA"
-type AllCombinations<S extends string> = Combination<StringToUnion<S>>;
+// type Combination<F extends string, U extends string = F> = F extends any
+//   ? F | `${F}${Combination<Exclude<U, F>>}`
+//   : "";
+// type C = Combination<"" | "A" | "B" | "C">; // "" | "A" | "AB" | "B" | "C" | "BC" | "CB" | "AC" | "ABC" | "ACB" | "CA" | "BA" | "BAC" | "BCA" | "CAB" | "CBA"
+// type AllCombinations<S extends string> = Combination<StringToUnion<S>>;
 
 type cases = [
   Expect<Equal<AllCombinations<"">, "">>,
